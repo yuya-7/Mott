fc6449920341a99044ccf7cd378e58b113110074
yuya-7 <yy0805.alwslv@icloud.com>
2020-05-17 23:11:38 +0900

    db設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+
+  root 'posts#index'
+
+  # devise_for :users, controllers: {
+  #   registrations: "users/registrations",
+  # }
+  # delete "destroy_user_session", to: "devise/sessions#destroy"
+  # devise_scope :user do
+  #   get "user_show", to: "users/registrations#show"
+  #   get "user_destroy", to: "users/sessions#index"
+  # end
+  # root 'items#index'
 end
