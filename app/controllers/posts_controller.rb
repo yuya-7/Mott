0064d5ae64a90105818e8f6bf719d87b8f759431
yuya-7 <yy0805.alwslv@icloud.com>
2020-05-21 16:00:43 +0900

    posts-view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
+  # before_action :set_post, only:[:show, :destroy, :edit, :update]
+
   def index
   end
+
+  def new
+    # @post = Post.new
+    # @post.images.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  # private
+
+  # def set_post
+  #   @post = Post.find(params[:id])
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+
+  # devise_for :users, controllers: {
+  #   registrations: "users/registrations",
+  # }
+  # delete "destroy_user_session", to: "devise/sessions#destroy"
+  # devise_scope :user do
+  #   get "user_show", to: "users/registrations#show"
+  #   get "user_destroy", to: "users/sessions#index"
+  # end
+  root 'posts#index'
+  resources :posts, only: [:new, :create, :show, :edit]
+  
 end
